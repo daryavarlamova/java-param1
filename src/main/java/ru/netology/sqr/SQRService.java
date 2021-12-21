c9ac25bab83f36939aba9788908270a5cdeae530
daryavarlamova <d.varlamova@icloud.com>
2021-12-21 09:49:45 -0800

    second

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -2,10 +2,12 @@ package ru.netology.sqr;
 
 public class SQRService {
     public int calculatecount(int min, int max) {
-        int count=0;
-        for (int i=10;i<100;i++) {
-        int temp=i*i;
-        if ((temp>=min) & (temp<=max)) count = count + 1;
+        int count = 0;
+        for (int i = 10; i < 100; i++) {
+            int temp = i * i;
+            if ((temp >= min) & (temp <= max)) {
+                count = count + 1;
+            }
         }
         return count;
     }
--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SQRServiceTest {
-    @CsvSource(value={"200,300,3","1,99,0","1,100,1","9801,9802,1","9802,9803,0"})
+    @CsvSource(value = {"200,300,3", "1,99,0", "1,100,1", "9801,9802,1", "9802,9803,0"})
     @ParameterizedTest
     void shouldCalculate(int min, int max, int expected) {
         SQRService service = new SQRService();
